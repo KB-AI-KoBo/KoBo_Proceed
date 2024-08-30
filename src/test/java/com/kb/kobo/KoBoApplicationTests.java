a95dc7b5695825b81adfc47813e60bcbe47ee9a4
ming9oo <mj10050203@gmail.com>
2024-08-30 18:58:07 +0900

    test api 삭제

---
+++ src/test/java/com/kb/kobo/KoBoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.kobo;
+
+import org.springframework.test.context.TestPropertySource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoBoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
