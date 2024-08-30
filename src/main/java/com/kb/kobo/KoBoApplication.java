059a6ae594bc7cf1e3c64e702c87dec0dc1b2bb3
ming9oo <mj10050203@gmail.com>
2024-08-30 18:44:55 +0900

    test 파일 수정

--- src/main/java/com/kb/kobo/KoBoApplication.java
+++ src/main/java/com/kb/kobo/KoBoApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class KoBoApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
         SpringApplication.run(KoBoApplication.class, args);
     }
 
